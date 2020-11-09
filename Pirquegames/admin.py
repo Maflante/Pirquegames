78894fa8c12e0c2663b8abebe6e1795f734b01a8
Maflante <rodrigoescala20@gmail.com>
2020-11-09 15:16:01 -0300

    intento de arreglo de badd

--- Pirquegames/admin.py
+++ Pirquegames/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
-# Register your models here.
-from Pirquegames.models import cuenta
-admin.site.register(cuenta)
+#from django.contrib import admin
+#Register your models here.
+#from .models import cuenta
+#admin.site.register(cuenta)
